bb0a444e42ae3de03c67cc8c6a0dbf682b529f5d
Dr-Banana <zhangxuanzhi10419@gmail.com>
2025-09-01 20:46:05 -0700

    use blog template for Netlify

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,15 +1,34 @@
+// stackbit.config.ts
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from '@stackbit/cms-git';
 
 export default defineStackbitConfig({
-    stackbitVersion: "~0.6.0",
-    nodeVersion: "18",
-    ssgName: "astro",
-    devCommand: "npm run dev",
-    buildCommand: "npm run build",
-    installCommand: "npm install",
-    modelExtensions: [
-        { name: "Page", type: "page", urlPath: "/{slug}" },
-        { name: "Blog", type: "page", urlPath: "/blog/{slug}" }
-    ],
-    postInstallCommand: "npm i --no-save @stackbit/types"
+    stackbitVersion: '~0.6.0',
+    ssgName: 'nextjs',
+    nodeVersion: '18',
+    contentSources: [
+        new GitContentSource({
+            rootPath: __dirname,
+            contentDirs: ['posts'],
+            models: [
+                {
+                    name: "Post",
+                    type: "page",
+                    urlPath: "/posts/{slug}",
+                    filePath: "posts/{slug}.mdx",
+                    fields: [
+                        { name: "title", type: "string", required: true, default: 'Post Title' }, 
+                        { name: "description", type: "string", default: 'Post description goes here' },
+                        { name: "date", type: "date", required: true },
+                    ]
+                  }
+            ],
+            assetsConfig: {
+                referenceType: 'static',
+                staticDir: 'public',
+                uploadDir: 'images',
+                publicPath: '/'
+            }
+        })
+    ]
 });
