5165a7913f8aa5fa3ac2e50652fd2b3b269f7296
Dr-Banana <zhangxuanzhi10419@gmail.com>
2025-09-01 19:37:54 -0700

    Setup Astro project with Visual Editor configuration

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,15 @@
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+    stackbitVersion: "~0.6.0",
+    nodeVersion: "18",
+    ssgName: "astro",
+    devCommand: "npm run dev",
+    buildCommand: "npm run build",
+    installCommand: "npm install",
+    modelExtensions: [
+        { name: "Page", type: "page", urlPath: "/{slug}" },
+        { name: "Blog", type: "page", urlPath: "/blog/{slug}" }
+    ],
+    postInstallCommand: "npm i --no-save @stackbit/types"
+});
